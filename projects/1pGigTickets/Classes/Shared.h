bcaa3db9aaa1faa2da88132f9bc32f97e13e3d6d
reenboog <alex.gievsky@gmail.com>
2013-09-19 01:43:48 +0700

    Added animation loader

---
+++ projects/1pGigTickets/Classes/Shared.h
@@ -0,0 +1,26 @@
+//
+//  Shared.h
+//  1pGigTickets
+//
+//  Created by Alex Gievsky on 18.09.13.
+//
+//
+
+#ifndef ___pGigTickets__Shared__
+#define ___pGigTickets__Shared__
+
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+using std::string;
+
+class Shared {
+private:
+    Shared() {};
+    ~Shared() {};
+public:
+    static Animation* loadAnimation(string file, string name);
+};
+
+#endif /* defined(___pGigTickets__Shared__) */
